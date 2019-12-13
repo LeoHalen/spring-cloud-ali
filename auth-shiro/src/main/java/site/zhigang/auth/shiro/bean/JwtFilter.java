a14cc171595f6bf74380fb1b3378cb473bb360a9
github-halen <halenmail@126.com>
2019-12-14 00:37:50 +0800

    The filter adds token validation and failed default response status code

--- auth-shiro/src/main/java/site/zhigang/auth/shiro/bean/JwtFilter.java
+++ auth-shiro/src/main/java/site/zhigang/auth/shiro/bean/JwtFilter.java
@@ -1,15 +1,25 @@
 package site.zhigang.auth.shiro.bean;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.web.filter.AccessControlFilter;
 import org.apache.shiro.web.util.WebUtils;
+import site.zhigang.auth.common.constants.RestCodeConstant;
+import site.zhigang.auth.common.response.BaseResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author Zg.Li · 2019/12/11
@@ -40,13 +50,49 @@ public class JwtFilter extends AccessControlFilter {
     @Override
     protected boolean onAccessDenied(ServletRequest req, ServletResponse res) throws Exception {
         HttpServletRequest httpServletRequest = WebUtils.toHttp(req);
-        String authorizaiton = httpServletRequest.getHeader(authorizationKey);
-        if (authorizaiton == null) {
-            authorizaiton = httpServletRequest.getParameter(authorizationKey);
+        try {
+            String authorization = httpServletRequest.getHeader(authorizationKey);
+            if (authorization == null) {
+                authorization = httpServletRequest.getParameter(authorizationKey);
+            }
+            if (Strings.isNullOrEmpty(authorization)) {
+                Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+                Map<String, String> headerMap = Maps.newHashMap();
+                while(headerNames.hasMoreElements()) {
+                    String headerName = headerNames.nextElement();
+                    headerMap.put(headerName, httpServletRequest.getHeader(headerName));
+                }
+                log.warn("Token cannot be obtained! Authorization: [{}], header: [{}]", authorizationKey, headerMap);
+                throw new AuthenticationException("Token cannot be obtained!");
+            }
+            // 先简单校验一下token 结构的合法性
+            // TODO
+            log.info("JWT Filter gets the token is [{}].", authorization);
+            JwtToken jwtToken = new JwtToken(authorization);
+            getSubject(req, res).login(jwtToken);
+        } catch (AuthenticationException e) {
+            log.info("Failed user login!");
+            onLoginFailed(res, e);
+            return false;
         }
-        if (Strings.isNullOrEmpty(authorizaiton)) {
+        return true;
+    }
 
-        }
-        return false;
+    /**
+     * token 校验失败返回 401 状态码
+     * @param response
+     * @param e
+     * @throws IOException
+     */
+    private void onLoginFailed(ServletResponse response, Exception e) throws IOException {
+        HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        BaseResponse resResult = new BaseResponse(RestCodeConstant.TOKEN_ERROR_CODE, e.getMessage());
+        ObjectMapper mapper = new ObjectMapper();
+        PrintWriter responseWriter = httpServletResponse.getWriter();
+        mapper.writeValue(responseWriter, resResult);
+        responseWriter.flush();
+        responseWriter.close();
     }
 }
