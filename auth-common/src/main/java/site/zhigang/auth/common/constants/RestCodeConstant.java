2a8902b81b67f0ef396ab58d5f98bd17955cfb03
github-halen <halenmail@126.com>
2019-12-14 00:38:51 +0800

    The filter adds token validation and failed default response status code

---
+++ auth-common/src/main/java/site/zhigang/auth/common/constants/RestCodeConstant.java
@@ -0,0 +1,9 @@
+package site.zhigang.auth.common.constants;
+
+/**
+ * @Author Zg.Li · 2019/12/14
+ */
+public class RestCodeConstant {
+	public static final int TOKEN_ERROR_CODE = 40101;
+	public static final int TOKEN_FORBIDDEN_CODE = 40301;
+}
